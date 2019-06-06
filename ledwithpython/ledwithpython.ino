4171270e6c2b620727b8f4dceecae887974b3a89
EdwardGithinji <50330712+EdwardGithinji@users.noreply.github.com>
2019-06-06 11:15:45 +0300

    Add files via upload

---
+++ ledwithpython/ledwithpython.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Hi!, I am Arduino");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char data;
+  while(Serial.available()){    
+    data = Serial.read();
+    if(data == '1'){
+      digitalWrite(LED_BUILTIN, HIGH); 
+    }
+  
+    else if(data == '0'){      
+      digitalWrite(LED_BUILTIN, LOW);
+    
+    }
+  
+  }
+
+
+}
